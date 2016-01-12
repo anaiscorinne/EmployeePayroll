1c40afa21cc85730ec3cf2648be288b4653ce099
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-12 14:30:45 -0500

    Employee Payroll Iteration 1

---
+++ app.rb
@@ -0,0 +1,20 @@
+require_relative("lib/employee.rb")
+require_relative("lib/hourly_employee.rb")
+require_relative("lib/salaried_employee.rb")
+require_relative("lib/multi_payment_employee.rb")
+
+josh = HourlyEmployee.new('Josh', 'nachoemail@example.com', 35, 50)
+puts josh.salary_statement
+puts josh.calculate_salary
+
+puts "--"
+
+nizar = SalariedEmployee.new('Nizar', 'starcraftrulez@gmail.com', 1000000)
+puts nizar.salary_statement
+puts nizar.calculate_salary
+
+puts "--"
+
+ted = MultiPaymentEmployee.new('Ted', 'fortranr0x@gmail.com', 60000, 275, 55)
+puts ted.salary_statement
+puts ted.calculate_salary
